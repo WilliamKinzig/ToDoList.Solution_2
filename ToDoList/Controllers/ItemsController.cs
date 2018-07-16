21bbfea4c906dc6d5b7f90bd09436d68bd8c5f36
WilliamKinzig <wippawill2@gmail.com>
2018-07-15 23:27:51 -0700

    update ItemsController.cs with the routes we want to use. We'll also update some of our views and their names.

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -6,17 +6,6 @@ namespace ToDoList.Controllers
 {
     public class ItemsController : Controller
     {
-// using [HttpGet()] [HttpPost()] [Route()]
-
-        //returns a view that shows the 'to do list'
-
-        // [HttpGet("/items")]
-        // public ActionResult Index()
-        // {
-        //     Item newItem = new Item(Request.Query["new-item"]); //takes input from form
-        //     return View(newItem);
-        // }
-
         [HttpGet("/items")]
         public ActionResult Index()
         {
@@ -24,22 +13,27 @@ namespace ToDoList.Controllers
             return View(allItems);
         }
 
-        //returns a veiw that shows a form for creating a new item
+        [HttpPost("/items")]
+        public ActionResult Create()
+        {
+            Item newItem = new Item(Request.Form["new-item"]);
+            List<Item> allItems = Item.GetAll();
+            return View("Index", allItems);
+        }
+
         [HttpGet("/items/new")]
         public ActionResult CreateForm()
         {
             return View();
         }
 
-        [HttpPost("/items")]
-        public ActionResult Create()
+        [HttpGet("/items/{id}")]
+        public ActionResult Details(int id)
         {
-          Item newItem = new Item (Request.Form["new-item"]);
-          newItem.Save();
-          // return View("Index", newItem);
-          List<Item> allItems = Item.GetAll();
-          return View("Index", allItems);
+            Item item = Item.Find(id);
+            return View(item);
         }
+        
 
         [HttpPost("/items/delete")]
         public ActionResult DeleteAll()
@@ -48,5 +42,59 @@ namespace ToDoList.Controllers
             return View();
         }
 
+
+
     }
 }
+
+
+
+
+// namespace ToDoList.Controllers
+// {
+//     public class ItemsController : Controller
+//     {
+// // using [HttpGet()] [HttpPost()] [Route()]
+//
+//         //returns a view that shows the 'to do list'
+//
+//         // [HttpGet("/items")]
+//         // public ActionResult Index()
+//         // {
+//         //     Item newItem = new Item(Request.Query["new-item"]); //takes input from form
+//         //     return View(newItem);
+//         // }
+//
+//         [HttpGet("/items")]
+//         public ActionResult Index()
+//         {
+//             List<Item> allItems = Item.GetAll();
+//             return View(allItems);
+//         }
+//
+//         //returns a veiw that shows a form for creating a new item
+//         [HttpGet("/items/new")]
+//         public ActionResult CreateForm()
+//         {
+//             return View();
+//         }
+//
+//         [HttpPost("/items")]
+//         public ActionResult Create()
+//         {
+//           Item newItem = new Item (Request.Form["new-item"]);
+//           newItem.Save();
+//           // return View("Index", newItem);
+//           List<Item> allItems = Item.GetAll();
+//           return View("Index", allItems);
+//         }
+//
+//         [HttpPost("/items/delete")]
+//         public ActionResult DeleteAll()
+//         {
+//             Item.ClearAll();
+//             return View();
+//         }
+//
+//     }
+// }
